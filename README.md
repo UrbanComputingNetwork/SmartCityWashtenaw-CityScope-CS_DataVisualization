36166beb8fe0e80472e17948889cb7d293e11dfe
doorleyr <doorleyr@tcd.ie>
2018-02-16 15:58:16 -0500

    Updated Readme and changed folder name

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # CS_DataVisualization
 
 Generic Data Visualization tools
+
+This tool requires input files:
+
+## pointData.json
+
+## zones.csv
+
+## flows.json
--- odMap.js
+++ odMap.js
@@ -26,8 +26,8 @@ var svg = d3.select("#odMapContainer").append("svg")
  
 svg.append("circle")
 .attr("r", outerRadius);
-d3.csv("./input/prepared/zones.csv", function(zones) {
-	d3.json("./input/prepared/matrix.json", function(matrix) {
+d3.csv("./input/Andorra/zones.csv", function(zones) {
+	d3.json("./input/Andorra/matrix.json", function(matrix) {
 	 
 	// Compute the chord layout.
 	layout.matrix(matrix);
